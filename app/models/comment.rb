ea0f506f6aab1c22de62d9118a9b42423308f888
Nicholas La Roux <larouxn@gmail.com>
2018-11-10 17:51:07 +0900

    Add model validation, require everything

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :idea
+
+  validates :user_name, :body, presence: true
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
   mount_uploader :picture, PictureUploader
   has_many :comments
+
+  validates :name, :description, :picture, presence: true
 end
