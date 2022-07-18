ef1bc7e81ad2a3cfef2b92d30cc8a4d90a22b5c2
Nicholas La Roux <larouxn@gmail.com>
2022-07-18 17:39:17 +0900

    [RuboCop] Extract content in locale file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,7 +30,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to(@comment, notice: "Comment was successfully created.") }
+        format.html { redirect_to(@comment, notice: I18n.t("controllers.comments.created.")) }
         format.json { render(:show, status: :created, location: @comment) }
       else
         format.html { render(:new) }
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
   def update
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to(@comment, notice: "Comment was successfully updated.") }
+        format.html { redirect_to(@comment, notice: I18n.t("controllers.comments.updated.")) }
         format.json { render(:show, status: :ok, location: @comment) }
       else
         format.html { render(:edit) }
@@ -59,7 +59,7 @@ class CommentsController < ApplicationController
     idea_id = @comment.idea_id
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to(idea_path(idea_id), notice: "Comment was successfully destroyed.") }
+      format.html { redirect_to(idea_path(idea_id), notice: I18n.t("controllers.comments.deleted.")) }
       format.json { head(:no_content) }
     end
   end
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -32,7 +32,7 @@ class IdeasController < ApplicationController
 
     respond_to do |format|
       if @idea.save
-        format.html { redirect_to(@idea, notice: "Idea was successfully created.") }
+        format.html { redirect_to(@idea, notice: I18n.t("controllers.ideas.created")) }
         format.json { render(:show, status: :created, location: @idea) }
       else
         format.html { render(:new) }
@@ -46,7 +46,7 @@ class IdeasController < ApplicationController
   def update
     respond_to do |format|
       if @idea.update(idea_params)
-        format.html { redirect_to(@idea, notice: "Idea was successfully updated.") }
+        format.html { redirect_to(@idea, notice: I18n.t("controllers.ideas.updated")) }
         format.json { render(:show, status: :ok, location: @idea) }
       else
         format.html { render(:edit) }
@@ -60,7 +60,7 @@ class IdeasController < ApplicationController
   def destroy
     @idea.destroy
     respond_to do |format|
-      format.html { redirect_to(ideas_url, notice: "Idea was successfully destroyed.") }
+      format.html { redirect_to(ideas_url, notice: I18n.t("controllers.ideas.deleted")) }
       format.json { head(:no_content) }
     end
   end
